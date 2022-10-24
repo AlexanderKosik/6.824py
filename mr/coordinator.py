0e6f2f0c2eadc2332e29b1a8d6f663245305ddb1
Alexander Kosik <akosik@t-online.de>
2022-10-24 08:31:38 +0200

    Add some more thoughts

---
+++ mr/coordinator.py
@@ -0,0 +1,12 @@
+#!python3
+
+# Reads all the input files passed as command line parameters
+
+# Stores this in a dict[filename, content]
+
+# Provides an TCP Server for RPC from workers
+# MIT example uses unix sockets. --> Refer to "How Linux works?"
+
+# Return value of RPC should propably be [filename, content]
+
+
---
+++ mr/worker.py
@@ -0,0 +1,10 @@
+#!python3
+
+# Can we load a go.so from python?
+
+# Worker
+# Request a filename and content (ignore filename) via RPC from coordinator
+# Call map in Worker
+# Call reduce in Worker
+
+# Unclear: When do we know when to call reduce on all workers?
